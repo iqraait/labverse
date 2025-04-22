29b7ea59dab8e39a4fed2a2cba4ee01d6d147443
iqraait <iqraautlityapp@gmail.com>
2025-04-22 12:58:10 +0530

    Initial commit with latest code1

--- myapp/admin.py
+++ myapp/admin.py
@@ -4,9 +4,9 @@ from .models import Payment
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ('email', 'amount', 'transcation_id', 'created_at','bank_transaction_id')
-    readonly_fields = ('transcation_id', 'bank_transaction_id', 'created_at')
     search_fields = ('full_name', 'transcation_id','contact')
     list_filter = ('created_at',)
+
 
 
 
