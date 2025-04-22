65ea9fcd1b1c86a5902b88c96d71837127d548de
iqraait <iqraautlityapp@gmail.com>
2025-04-22 14:09:34 +0530

    Initial commit with latest code3

--- myapp/models.py
+++ myapp/models.py
@@ -47,7 +47,7 @@ class Payment(models.Model):
 
     job_title = models.CharField(max_length=200,blank=True)
 
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     meals = models.CharField(max_length=20,choices=CATEGORY_CHOICES_MEALS)
 
